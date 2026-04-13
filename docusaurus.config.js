ba5bfcb31efa44707c464e025967d80442acb148
tomirodeghiero <tomyrodeghiero@gmail.com>
2026-04-12 22:57:12 -0300

    Rediseñar interfaz Docusaurus y actualizar identidad visual UNRC

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,15 @@
 // @ts-check
+const { themes } = require('prism-react-renderer');
+
+const currentYear = new Date().getFullYear();
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Testing de Software - Documentacion',
-  tagline: 'Trabajos practicos',
+  title: 'Testing de Software - Documentacion Academica',
+  tagline: 'Trabajos practicos y evidencia tecnica',
   url: 'http://localhost',
   baseUrl: '/',
+  favicon: 'img/unrc.png',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   i18n: {
@@ -28,9 +32,29 @@ const config = {
     ]
   ],
   themeConfig: {
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true
+    },
+    docs: {
+      sidebar: {
+        hideable: true,
+        autoCollapseCategories: true
+      }
+    },
     navbar: {
       title: 'Testing de Software',
+      hideOnScroll: true,
+      logo: {
+        alt: 'Escudo UNRC',
+        src: 'img/unrc.png'
+      },
       items: [
+        {
+          to: '/',
+          label: 'Portada',
+          position: 'left'
+        },
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
@@ -38,6 +62,39 @@ const config = {
           label: 'Documentacion'
         }
       ]
+    },
+    footer: {
+      style: 'light',
+      links: [
+        {
+          title: 'Contenido',
+          items: [
+            {
+              label: 'Indice principal',
+              to: '/'
+            },
+            {
+              label: 'TP1 a TP9',
+              to: '/tp1/'
+            }
+          ]
+        },
+        {
+          title: 'Catedra',
+          items: [
+            {
+              label: 'Testing de Software',
+              to: '/'
+            }
+          ]
+        }
+      ],
+      copyright: `(c) ${currentYear} Documentacion academica de trabajos practicos.`
+    },
+    prism: {
+      theme: themes.github,
+      darkTheme: themes.dracula,
+      additionalLanguages: ['java', 'bash', 'markdown']
     }
   }
 };
